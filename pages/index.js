884815ab83218fce1b340d2abb3e7372e3357616
David Hamilton <david@thebanditdave.com>
2023-04-22 23:29:29 -0500

    fix typo

--- pages/index.js
+++ pages/index.js
@@ -108,7 +108,7 @@ const Home = () => {
             <h1>AI Avatar Generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>Turn me into anyone you want! Make sure you refer to me as "thebanditdave" in the prompt"</h2>
+            <h2>Turn me into anyone you want! Make sure you refer to me as "thebanditdave" in the prompt</h2>
           </div>
           <div className="prompt-container">
             <input className="prompt-box" value={input} onChange={onChange} />
